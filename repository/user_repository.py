d6400ee21ab052afda66f8e4b6bad16f12f5e5bd
unknown <magizhtechnologies24@gmail.com>
2026-06-06 10:17:30 +0530

    create and read

--- repository/user_repository.py
+++ repository/user_repository.py
@@ -5,8 +5,13 @@ class UserRepository:
     def __init__(self):
         self.users = []
 
+
+    def get_all_users(self):
+        return self.users
+
     def add_user(self, user: User):
         self.users.append(user)
+        return user
 
     def get_user(self, user_id: int) -> User:
         for user in self.users:
